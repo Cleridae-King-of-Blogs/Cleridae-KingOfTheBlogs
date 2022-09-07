3ddcad277dca2127dd5796f41131be01b893b4f1
eugeneshine1 <eugeneshine1@gmail.com>
2022-09-07 08:32:10 -0400

    about page

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,9 +1,53 @@
 import "./about.css";
+const PF = "http://localhost:4000/images/";
 
 export default function About() {
   return (
-    <div>
-      <p className="about">About us!</p>
-    </div>
+    <div className="about">
+      <span className="aboutTitle">About us!</span>
+      <div className="aboutItem">
+        
+        <div className="aboutRandy">      
+        <img
+          className="randyImage"
+          src={PF + "randy.png"}
+          alt=""
+        />
+        <span>
+          This is the blurb section.
+        </span>
+        </div>
+        <div className="aboutMary">
+        <img
+          className="maryImage"
+          src={PF + "mary.png"}
+          alt=""
+        />
+        <span>
+          This is the blurb section.
+        </span>
+        </div>
+        <div className="aboutKyle">
+        <img
+          className="kyleImage"
+          src={PF + "kyle.jpeg"}
+          alt=""
+        />
+        <span>
+          This is the blurb section.
+        </span>
+        </div>
+        <div className="aboutEugene">
+        <img
+          className="eugeneImage"
+          src={PF + "eugene.png"}
+          alt=""
+        />
+        <span>
+          This is the blurb section.
+        </span>
+        </div>
+      </div>
+      </div>
   );
 }
